77a4de504c7eea3055c5edfbf04855500514b5db
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-09-28 16:37:09 +0500

    123213

--- server.js
+++ server.js
@@ -4,11 +4,35 @@ const compression = require('compression')
 const morgan = require('morgan')
 const path = require('path')
 
+const normalizePort = (port) => parseInt(port, 10)
+const PORT = normalizePort(process.env.PORT || 5000)
+
 const app = express()
 const dev = app.get('env') !== 'production'
 
 if (!dev) {
+  console.log('PROD')
+
   app.disable('x-powered-by')
   app.use(compression())
   app.use(morgan('common'))
+
+  app.use(express.static(path.resolve(__dirname, 'build')))
+
+  //console.log(path.resolve(__dirname, 'build', 'index.html'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
 }
+
+if (dev) {
+  console.log('DEV')
+  app.use(morgan('dev'))
+}
+
+const server = createServer(app)
+server.listen(PORT, (err) => {
+  if (err) throw err
+
+  console.log(`Server started on port ${PORT}`)
+})
