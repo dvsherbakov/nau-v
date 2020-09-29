2248ed86e8a716b0fdf1e367029f00173cd5d7fd
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-09-29 21:12:59 +0500

    fix api bug

--- src/components/Check.js
+++ src/components/Check.js
@@ -1,17 +1,6 @@
 import React from 'react'
-import Api from '../Api'
 
 export const Check = () => {
-  async function getProducts() {
-    //jwtApi.register({ email: 'ds.tura.ru', password: 'gzaktpf6' })
-    await jwtApi.login({ login: 'ds.tura.ru', password: 'gzaktpf6' })
-
-    const res = await jwtApi.getProducts()
-
-    return res
-  }
-  const jwtApi = new Api()
-  getProducts()
   return (
     <>
       <h2>Check Page component</h2>
