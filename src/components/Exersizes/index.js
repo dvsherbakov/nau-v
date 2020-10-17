06bea982cf51c6f83d8ad6aa548d709df4587e4c
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-10-17 23:18:02 +0500

    exersizes some styles

--- src/components/Exersizes/index.js
+++ src/components/Exersizes/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { exersizes } from './exlist'
+import './styles.css'
 
 export const Exersizes = () => {
+  const classes = new Set(exersizes.map((e) => e.klass))
+  console.log(classes)
   const resList = exersizes.map((e) => (
-    <div key={e.id}>
-      <div>{e.klass} класс</div>
-      <div>Тэги: {e.tags.join(' ')}</div>
-      <div>{e.text}</div>
+    <div key={e.id} className="exersizes__unit">
+      <div className="exersizes__header-container">
+        <div className="exersizes__header-head">
+          <div className="exersizes__header-item">{e.klass} класс</div>
+          <div className="exersizes__header-item">Тэги: {e.tags.join(' ')}</div>
+        </div>
+        <div>Шифр: {e.id}</div>
+      </div>
+      <div className="exersizes__body">{e.text}</div>
     </div>
   ))
   return (
