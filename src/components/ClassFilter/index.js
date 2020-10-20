6c6b9a67a1f31c6cd3fdea8342ee9659b5edcb43
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-10-20 17:23:38 +0500

    add tags filter

--- src/components/ClassFilter/index.js
+++ src/components/ClassFilter/index.js
@@ -3,20 +3,64 @@ import React from 'react'
 import './style.css'
 
 export const ClassFilter = (props) => {
-  const filtress = props.cl
-    ? props.cl.map((e, idx) => {
+  const ClassesClickHandler = (e) => {
+    const newValue = {
+      ...props.Classes[e.target.id],
+      isSelected: !props.Classes[e.target.id].isSelected,
+    }
+    const newArray = [
+      ...props.Classes.slice(0, +e.target.id),
+      newValue,
+      ...props.Classes.slice(+e.target.id + 1),
+    ]
+    props.SetClasses(newArray)
+  }
+
+  const TagsClickHandler = (e) => {
+    const newValue = {
+      ...props.Tags[e.target.id],
+      isSelected: !props.Tags[e.target.id].isSelected,
+    }
+    const newArray = [
+      ...props.Tags.slice(0, +e.target.id),
+      newValue,
+      ...props.Tags.slice(+e.target.id + 1),
+    ]
+    props.SetTags(newArray)
+  }
+
+  const filtress = props.Classes
+    ? props.Classes.map((e, idx) => {
         return (
-          <div key={idx} className="classfltr__item">
-            {e}
+          <div
+            id={e.id}
+            key={idx}
+            className={
+              e.isSelected
+                ? 'classfltr__item'
+                : 'classfltr__item classfltr__unselected'
+            }
+            onClick={ClassesClickHandler}
+          >
+            {e.klass}
           </div>
         )
       })
     : []
-  const tags = props.tg
-    ? props.tg.map((e, idx) => {
+  const tags = props.Tags
+    ? props.Tags.map((e, idx) => {
         return (
-          <div key={idx} className="tagsfltr__item">
-            {e}
+          <div
+            id={e.id}
+            key={idx}
+            className={
+              e.isSelected
+                ? 'tagsfltr__item'
+                : 'tagsfltr__item classfltr__unselected'
+            }
+            onClick={TagsClickHandler}
+          >
+            {e.tag}
           </div>
         )
       })
--- src/components/Exersizes/index.js
+++ src/components/Exersizes/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ClassFilter } from '../ClassFilter'
 import { exersizes } from './exlist'
 import './styles.css'
@@ -8,25 +8,57 @@ export const Exersizes = () => {
     return self.indexOf(value) === index
   }
 
-  const classes = exersizes.map((e) => e.klass).filter(onlyUnique)
-  const tags = [].concat(...exersizes.map((e) => e.tags)).filter(onlyUnique)
+  const [classes, setClasses] = useState(
+    exersizes
+      .map((e) => e.klass)
+      .filter(onlyUnique)
+      .map((el, id) => {
+        return { id, klass: el, isSelected: true }
+      })
+  )
 
-  const resList = exersizes.map((e) => (
-    <div key={e.id} className="exersizes__unit">
-      <div className="exersizes__header-container">
-        <div className="exersizes__header-head">
-          <div className="exersizes__header-item">{e.klass} класс</div>
-          <div className="exersizes__header-item">Тэги: {e.tags.join(' ')}</div>
+  const [tags, setTags] = useState(
+    []
+      .concat(...exersizes.map((e) => e.tags))
+      .filter(onlyUnique)
+      .map((el, id) => {
+        return { id, tag: el, isSelected: true }
+      })
+  )
+
+  const tagIsSelected = (tgs) => {
+    const filtered = tags.filter((el) => tgs.includes(el.tag))
+    console.log(filtered)
+    return filtered && filtered[0].isSelected
+  }
+
+  //ToDo add class filter
+
+  const resList = exersizes
+    .filter((el) => tagIsSelected(el.tags))
+    .map((e) => (
+      <div key={e.id} className="exersizes__unit">
+        <div className="exersizes__header-container">
+          <div className="exersizes__header-head">
+            <div className="exersizes__header-item">{e.klass} класс</div>
+            <div className="exersizes__header-item">
+              Тэги: {e.tags.join(' ')}
+            </div>
+          </div>
+          <div>Шифр: {e.id}</div>
         </div>
-        <div>Шифр: {e.id}</div>
+        <div className="exersizes__body">{e.text}</div>
       </div>
-      <div className="exersizes__body">{e.text}</div>
-    </div>
-  ))
+    ))
   return (
     <div>
       <h1>Задачи и упражнения</h1>
-      <ClassFilter cl={classes} tg={tags} />
+      <ClassFilter
+        Classes={classes}
+        SetClasses={setClasses}
+        Tags={tags}
+        SetTags={setTags}
+      />
       <div>{resList}</div>
     </div>
   )
