0c43fc5159e3560fb4689f19531f9bdcdc921dc2
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-11-07 19:52:11 +0500

    some changes

--- src/components/UserTestStat/UserTestStat.js
+++ src/components/UserTestStat/UserTestStat.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles.css'
 
 export const UserTestStat = (props) => {
+  const [isCompact, setCompact] = useState(true)
+
   console.log(props)
 
+  const wrapperClick = () => {
+    setCompact(!isCompact)
+  }
+
   if (!props || !props.result.length) return null
-  return (
-    <span className="test-stat__wrapper">
-      <div className="test-stat"></div>
-    </span>
-  )
+  if (isCompact) {
+    return (
+      <span className="test-stat__wrapper" onClick={wrapperClick}>
+        <div className="test-stat"></div>
+      </span>
+    )
+  } else {
+    return <div className={'test-stat__container'}></div>
+  }
 }
