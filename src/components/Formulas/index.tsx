1782a3895001fc974b2d6e8dccd08d0998ba9a26
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-12-17 17:43:01 +0500

    sch

--- src/components/Formulas/index.tsx
+++ src/components/Formulas/index.tsx
@@ -1,7 +1,6 @@
 import { Formulas } from './Formulas'
 import { Formula } from './Formula'
-import { IFormula } from './types'
+
 import FormulasList from './FormulasList'
 
 export { Formulas, Formula, FormulasList }
-export { IFormula }
