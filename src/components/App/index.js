6fe7e8a750dc812ecda8ba82357a3e442e4e0faa
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-12-22 16:48:46 +0500

    fix connect error

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,3 @@
 import App from './App'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import * as actions from '../../store/actions'
 
-const { changeFirstName, changeLastName } = actions
-
-const putStateToProps = (state) => ({
-  firstName: state.firstName,
-  lastName: state.lastName,
-})
-
-const putActionsToProps = (dispatch) => {
-  return {
-    changeFirstName: bindActionCreators(changeFirstName, dispatch),
-    changeLastName: bindActionCreators(changeLastName, dispatch),
-  }
-}
-
-export default connect(putStateToProps, putActionsToProps)(App)
+export default App
