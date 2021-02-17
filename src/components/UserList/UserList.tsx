44985de0a0c7c9eb5e9d4335d17fdfdd1d9ed48d
Dmitry Sherbakov <dvsherbakov@gmail.com>
2021-02-17 12:29:09 +0500

    some changes

---
+++ src/components/UserList/UserList.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+import './userlist.css'
+
+export const UserList = () => {
+  return (
+    <Fragment>
+      <div className="userlist">
+        <h2>Список пользователей</h2>
+      </div>
+    </Fragment>
+  )
+}
