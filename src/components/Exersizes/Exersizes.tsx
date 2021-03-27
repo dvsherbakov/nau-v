c834a533a96df47956b580b89ff976c6f64107ab
Dmitry Sherbakov <dvsherbakov@gmail.com>
2021-03-27 22:03:27 +0500

    exersises filteres

--- src/components/Exersizes/Exersizes.tsx
+++ src/components/Exersizes/Exersizes.tsx
@@ -1,13 +1,41 @@
 import React, { FC } from 'react'
 import { Exersise } from './Exersise'
 import { exersises } from './ExersisesList'
+//import { IExersise } from './types'
 
 export const Exersizes: FC = () => {
+  function onlyUnique<T>(value: T, index: number, self: T[]) {
+    return self.indexOf(value) === index
+  }
+
+  // var tmp: string[] = []
+  const tags: string[] = ['Все']
+    .concat(...exersises.map((e) => e.tags))
+    .filter(onlyUnique)
+
+  const classes: string[] = ['Все']
+    .concat(...exersises.map((e) => e.klass.toString()))
+    .filter(onlyUnique)
+
   return (
-    <div className="exersises">
-      {exersises.map((exersise) => (
-        <Exersise {...exersise} />
-      ))}
+    <div className="ex-form">
+      <div className="ex-form__flex container">
+        <select>
+          {tags.map((tag) => (
+            <option value={tag}>{tag}</option>
+          ))}
+        </select>
+        <select>
+          {classes.map((tag) => (
+            <option value={tag}>{tag}</option>
+          ))}
+        </select>
+      </div>
+      <div className="exersises">
+        {exersises.map((exersise) => (
+          <Exersise {...exersise} />
+        ))}
+      </div>
     </div>
   )
 }
